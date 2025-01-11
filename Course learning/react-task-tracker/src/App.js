35268d37db6e170663e0d615ad18663d96fe72f2
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-01-11 20:49:08 +0200

    React(ing)

--- Course learning/react-task-tracker/src/App.js
+++ Course learning/react-task-tracker/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
+])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{color: 'red', backgroundColor: 'black'}}>Hello</h1>
+      <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
 
+/*
+const App = () => {
+  return (
+    <div className="container">
+      <Header title='This is a prop'/>
+    </div>
+  );
+};
+*/
 export default App;
