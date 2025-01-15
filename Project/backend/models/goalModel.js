6191d670010eede0c33b1e68781fc59a2a58d8aa
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-01-15 16:31:08 +0200

    Goals set to users

--- Project/backend/models/goalModel.js
+++ Project/backend/models/goalModel.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const goalSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        ref: 'User',
+    },
     text: {
         type: String,
         required: [true, 'text is required']
