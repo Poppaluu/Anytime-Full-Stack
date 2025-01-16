c3f60fe36331c5f1535d8e8ca22246463e61b8c0
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-01-16 13:52:36 +0200

    redux, axios, localstorage, jwt

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
