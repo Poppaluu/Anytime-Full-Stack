fdd8b3c804912f794546cd5eda79e70558b3e57e
Matias Tarvainen <tarvainen.matias@gmail.com>
2025-01-16 15:11:34 +0200

    lecture finished by making goals
    adding goals and deleting goals

--- Project/backend/controllers/goalController.js
+++ Project/backend/controllers/goalController.js
@@ -38,15 +38,13 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error("User not found")
     }
 
     //make sure that logged user mathces the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error("User not authorized")
     }
@@ -69,15 +67,13 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error("User not found")
     }
 
     //make sure that logged user mathces the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error("User not authorized")
     }
